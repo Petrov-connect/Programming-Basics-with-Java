1288cd16eb7e9bd26293c7a9734f2cf48a0451e8
ADMIN <petrov.connect@gmail.com>
2020-12-12 19:01:57 +0200

    java code

---
+++ CareofPuppy.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// created by J.M.
+public class CareofPuppy {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int food = Integer.parseInt(scan.nextLine())*1000;
+        double sum = 0.0;
+        for (;;){
+        String eat = scan.nextLine();
+        if (eat.equals("Adopted")){
+            break;
+        }
+        double n = Double.parseDouble(eat);
+        sum+=n;
+        }
+        if(food>=sum){
+            System.out.printf("Food is enough! Leftovers: %.0f grams.",food-sum);
+        }else {
+            System.out.printf("Food is not enough. You need %.0f grams more.",sum-food);
+        }
+    }
+}
