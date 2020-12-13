06eb15e0b7630e3d92d3b863c52075fd00aca0cd
ADMIN <petrov.connect@gmail.com>
2020-12-13 18:53:54 +0200

    java code

---
+++ CharityCampaign.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+// created by J.M.
+public class CharityCampaign {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int days = Integer.parseInt(scan.nextLine());
+        int cooks = Integer.parseInt(scan.nextLine());
+        int cake = Integer.parseInt(scan.nextLine());
+        int corr = Integer.parseInt(scan.nextLine());
+        int pancake = Integer.parseInt(scan.nextLine());
+
+        System.out.printf("%.2f", (days * cooks * (cake * 45 + corr * 5.80 + pancake * 3.20))*0.875);
+    }
+}
