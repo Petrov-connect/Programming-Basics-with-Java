bc4a29720879cf2dd2ee948a6aa374ce62702857
ADMIN <petrov.connect@gmail.com>
2020-12-14 15:40:46 +0200

    java code

---
+++ Coins.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+// created by J.M.
+public class Coins {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double sum = Double.parseDouble(scan.nextLine())*100;
+        int count = 0;
+        while(sum>=200){
+            sum-=200;
+            count++;
+        }while(sum>=100){
+            sum-=100;
+            count++;
+        }while(sum>=50){
+            sum-=50;
+            count++;
+        }while(sum>=20){
+            sum-=20;
+            count++;
+        }while(sum>=10){
+            sum-=10;
+            count++;
+        }while(sum>=5){
+            sum-=5;
+            count++;
+        }while(sum>=2){
+            sum-=2;
+            count++;
+        }while(sum>=1){
+            sum-=1;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
