5219b439b74bdbf46456dcee2b0e87b0e38927ee
ADMIN <petrov.connect@gmail.com>
2020-12-14 15:31:25 +0200

    java code

---
+++ Clock.java
@@ -0,0 +1,10 @@
+public class Clock {
+    // created by J.M.
+    public static void main(String[] args) {
+        for (int i =0; i<=23 ; i++){
+           for(int a = 0; a<=59; a++){
+               System.out.printf("%d:%d%n",i,a);
+           }
+       }
+    }
+}
