d57a3030eb1ca3aa699983e9d7b0021eccc17352
ADMIN <petrov.connect@gmail.com>
2020-12-14 15:38:03 +0200

    java code

---
+++ ClockPart2.java
@@ -0,0 +1,12 @@
+public class ClockPart2 {
+    // created by J.M.
+    public static void main(String[] args) {
+        for (int i =0; i<=23 ; i++){
+            for(int a = 0; a<=59; a++){
+                for(int b=0; b<=59; b++) {
+                    System.out.printf("%d : %d : %d%n", i, a, b);
+                }
+            }
+        }
+    }
+}
