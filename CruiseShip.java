bc0408dd8db5469499b2936d445b1aceec99add1
Petrov-connect <Petrov.connect@gmail.com>
2020-12-15 17:05:08 +0200

    java code

---
+++ CruiseShip.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+// created by J.M.
+public class CruiseShip {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String cruiz = scan.nextLine();
+        String kabin = scan.nextLine();
+        int nights = Integer.parseInt(scan.nextLine());
+        double price=0;
+
+        switch(cruiz){
+            case"Mediterranean":
+                switch(kabin){
+                    case"standard cabin":
+                        price=27.50;
+                        break;
+                    case"cabin with balcony":
+                        price=30.20;
+                        break;
+                    case"apartment":
+                        price=40.50;
+                        break;
+                }
+                break;
+            case"Adriatic":
+                switch(kabin) {
+                    case "standard cabin":
+                        price = 22.99;
+                        break;
+                    case "cabin with balcony":
+                        price = 25.00;
+                        break;
+                    case "apartment":
+                        price = 34.99;
+                        break;
+                }
+                break;
+            case"Aegean":
+                switch(kabin) {
+                    case "standard cabin":
+                        price = 23.00;
+                        break;
+                    case "cabin with balcony":
+                        price = 26.60;
+                        break;
+                    case "apartment":
+                        price = 39.80;
+                        break;
+                }
+                break;
+        }
+        double total=nights*price*4;
+        if (nights>7){
+            total=total*0.75;
+        }
+        System.out.printf("Annie's holiday in the %s sea costs %.2f lv.",cruiz,total);
+    }
+}
