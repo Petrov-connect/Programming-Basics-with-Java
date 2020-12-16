a5fe0cc38e051befffe2c5bdcad837b61f1cba03
Petrov-connect <Petrov.connect@gmail.com>
2020-12-16 17:36:42 +0200

    java code

---
+++ Darts.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+// created by J.M.
+public class Darts {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        boolean points301 = false;
+        int points = 301; int count=0; int countFalse=0;
+        String sector=scan.nextLine();
+        while (!sector.equals("Retire")) {
+            int shot = Integer.parseInt(scan.nextLine());
+            switch ((sector)) {
+                case "Single": shot*=1; break;
+                case "Double": shot*=2; break;
+                case "Triple": shot*=3; break;
+            }
+            if(shot>points){
+                countFalse++;
+                sector=scan.nextLine();
+                continue;
+            }
+            points-=shot; count++;
+            if(points==0){
+                points301=true; break;
+            }
+            sector=scan.nextLine();
+        }
+        if(points301){
+            System.out.printf("%s won the leg with %d shots.",name,count);
+        }else{
+            System.out.printf("%s retired after %d unsuccessful shots.",name,countFalse);
+        }
+    }
+}
