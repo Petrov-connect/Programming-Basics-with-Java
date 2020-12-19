cc7c501886752a9c54cce91cda7a29f78cf5ad37
Petrov-connect <Petrov.connect@gmail.com>
2020-12-19 22:48:59 +0200

    java code

---
+++ EasterTrip.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+// created by J.M.
+public class EasterTrip {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String destination = scan.nextLine();
+        String date = scan.nextLine();
+        int nights = Integer.parseInt(scan.nextLine());
+        double price=0;
+        switch(destination){
+            case"France":
+                switch(date){
+                    case"21-23": price=30.0; break;
+                    case"24-27": price=35.0; break;
+                    case"28-31": price=40.0; break;
+                }
+                break;
+            case"Italy":
+                switch(date){
+                    case"21-23": price=28.0; break;
+                    case"24-27": price=32.0; break;
+                    case"28-31": price=39.0; break;
+                }
+                break;
+            case"Germany":
+                switch(date){
+                    case"21-23": price=32.0; break;
+                    case"24-27": price=37.0; break;
+                    case"28-31": price=43.0; break;
+                }
+                break;
+        }
+            double totalPrice=nights*price;
+        System.out.printf("Easter trip to %s : %.2f leva.",destination,totalPrice);
+    }
+}
