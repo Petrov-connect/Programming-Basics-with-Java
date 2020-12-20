41da65d2953ac66288df0f0323908225b0cf82cb
Petrov-connect <Petrov.connect@gmail.com>
2020-12-20 13:59:29 +0200

    java code

---
+++ ExcellentResult.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+// created by J.M.
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double rating = Double.parseDouble(scan.nextLine());
+        if( rating >= 5.50 ){
+            System.out.println("Excellent!");
+        }
+    }
+}
