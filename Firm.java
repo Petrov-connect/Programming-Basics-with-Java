fa7edfb00e6363bbef157673d602c642b36766df
Petrov-connect <Petrov.connect@gmail.com>
2020-12-22 18:35:57 +0200

    java code

---
+++ Firm.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+// created by J.M.
+public class Firm {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int needHours = Integer.parseInt(scan.nextLine());
+        int daysForWork = Integer.parseInt(scan.nextLine());
+        int employees = Integer.parseInt(scan.nextLine());
+        double hours = Math.floor((employees * 2 * daysForWork) + (daysForWork * 0.9 * 8));
+        if (hours >= needHours) {
+            double left = hours - needHours;
+            System.out.printf("Yes!%.0f hours left.", left);
+        } else {
+            double needed = needHours - hours;
+            System.out.printf("Not enough time!%.0f hours needed.", needed);
+        }
+    }
+}
