dc0944602ec1a746c30756f406bf9d7ad56df2af
Petrov-connect <Petrov.connect@gmail.com>
2020-12-26 13:10:28 +0200

    java code

---
+++ FuelTankPart2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+// created by J.M.
+public class FuelTankPart2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String fuel = scan.nextLine();
+        double quantity = Double.parseDouble(scan.nextLine());
+        String card = scan.nextLine();
+        double price = 0.0;
+        if (fuel.equals("Gasoline")) {
+            price = 2.22;
+            if (card.equals("Yes")) {
+                price -= 0.18;
+            }
+        }
+        if (fuel.equals("Diesel")) {
+            price = 2.33;
+            if (card.equals("Yes")) {
+                price -= 0.12;
+            }
+        }
+        if (fuel.equals("Gas")) {
+            price = 0.93;
+            if (card.equals("Yes")) {
+                price -= 0.08;
+            }
+        }
+        double totalPrice = quantity * price;
+
+        if (quantity > 25) {
+            totalPrice *= 0.9;
+        } else if (quantity >= 20 && quantity <= 25) {
+            totalPrice *= 0.92;
+        }
+        System.out.printf("%.2f lv.", totalPrice);
+    }
+}
