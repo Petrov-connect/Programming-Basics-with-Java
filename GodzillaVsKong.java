afc63ffd63f5bcb36ec73d5ef445be87d9e9e9f2
Petrov-connect <Petrov.connect@gmail.com>
2020-12-27 19:53:47 +0200

    java code

---
+++ GodzillaVsKong.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// created by J.M.
+public class GodzillaVsKong {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double budget = Double.parseDouble(scan.nextLine());
+        int actors = Integer.parseInt(scan.nextLine());
+        double priceCostum = Double.parseDouble(scan.nextLine());
+        double decor = budget * 0.1;
+        if (actors > 150) {
+            priceCostum *= 0.9;
+        }
+        double total = (priceCostum * actors) + decor;
+        if (total > budget) {
+            System.out.println("Not enough money!");
+            System.out.printf("Wingard needs %.2f leva more.", total - budget);
+        } else {
+            System.out.println("Action!");
+            System.out.printf("Wingard starts filming with %.2f leva left.", budget - total);
+        }
+    }
+}
